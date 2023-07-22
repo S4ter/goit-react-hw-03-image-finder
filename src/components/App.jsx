de72f0e92a6b57a5ae378c39eb253e68681b6f38
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-07-22 03:27:05 +0200

    styling, new components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,44 +1,38 @@
 import React, { Component } from 'react';
-
-class SearchBar extends Component {
-  render() {
-    const { inputSearch, handleSubmit } = this.props;
-
-    return (
-      <header className="searchbar">
-        <form className="form" onSubmit={handleSubmit}>
-          <input
-            className="input"
-            type="text"
-            value={inputSearch}
-            onChange={() => {}}
-            placeholder="Search images and photos"
-          />
-          <button type="submit" className="button">
-            <span className="button-label">Search</span>
-          </button>
-        </form>
-      </header>
-    );
-  }
-}
+import Loader from './Loader/Loader';
+import RenderList from './RenderList/RenderList';
+import SearchBar from './SearchBar/SearchBar';
+import './App.styles.css';
 
 class Gallery extends Component {
   state = {
     images: [],
     inputSearch: '',
-    limit: '12',
+    limit: 12,
     page: '1',
+    isLoading: false,
+    isModalOpen: false,
     API_KEY: '36730001-9966eb2ff0700192767337e13',
   };
 
   async componentDidMount() {
-    this.fetchFromApi();
+    if (this.state.inputSearch.length === 0 && this.state.images.length > 0) {
+      this.setState({ images: [] });
+    }
   }
-  async componentDidUpdate() {}
+  async componentDidUpdate(prevProps, prevState) {
+    if (prevState.limit !== this.state.limit) {
+      this.fetchFromApi();
+    }
+  }
+
+  handleChange = e => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
 
   fetchFromApi = async () => {
-    console.log('test');
+    this.setState({ isLoading: true });
     try {
       const { inputSearch, limit, page, API_KEY } = this.state;
       const response = await fetch(
@@ -49,22 +43,45 @@ class Gallery extends Component {
       this.setState(prevState => ({ ...prevState, images: data.hits }));
     } catch (error) {
       console.log('error', error);
+    } finally {
+      this.setState({ isLoading: false });
     }
+  };
+  changeLimit = () => {
+    this.setState(prevState => ({ ...prevState, limit: prevState.limit + 10 }));
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log('submit');
-    this.fetchFromApi();
+    if (this.state.inputSearch.length === 0) {
+      this.setState({ images: [] });
+      alert('Search input cannot me empty');
+    } else {
+      this.fetchFromApi();
+    }
   };
+
   render() {
-    console.log(this.state.images);
     return (
-      <div>
+      <div className="app">
         <SearchBar
           inputSearch={this.state.inputSearch}
           handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
         ></SearchBar>
-        <ul className="Gallery"> GALERIA</ul>
+
+        {this.state.isLoading ? (
+          <Loader />
+        ) : (
+          <RenderList images={this.state.images} />
+        )}
+        <button
+          type="button"
+          name="showMore"
+          onClick={this.changeLimit}
+          className="more_button"
+        >
+          Show More
+        </button>
       </div>
     );
   }
